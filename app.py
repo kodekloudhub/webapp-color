a7771de37e382b2c2e76293c858cb5a70f9a0d43
Mumshad Mannambeth <mmumshad@gmail.com>
2018-10-17 12:40:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,31 +16,52 @@ color_codes = {
     "darkblue": "#130f40"
 }
 
-color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue2","darkblue","pink"])
+SUPPORTED_COLORS = ",".join(color_codes.keys())
+
+# Get color from Environment variable
+COLOR_FROM_ENV = os.environ.get('APP_COLOR')
+# Generate a random color
+COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink"])
+
 
 @app.route("/")
 def main():
-    #return 'Hello'
-    print(args.color)
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[args.color])
+    # return 'Hello'
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[COLOR])
 
-@app.route('/color/<new_color>')
-def new_color(new_color):
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
-
-@app.route('/read_file')
-def read_file():
-    f = open("/data/testfile.txt")
-    contents = f.read()
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[args.color])
 
 if __name__ == "__main__":
+
+    print(" This is a sample web application that displays a colored background. \n"
+          " A color can be specified in two ways. \n"
+          "\n"
+          " 1. As a command line argument with --color as the argument. Accepts one of " + SUPPORTED_COLORS + " \n"
+          " 2. As an Environment variable APP_COLOR. Accepts one of " + SUPPORTED_COLORS + " \n"
+          " 3. If none of the above then a random color is picked from the above list. \n"
+          " Note: Command line argument precedes over environment variable.\n"
+          "\n"
+          "")
+
     # Check for Command Line Parameters for color
     parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False, default='blue')
+    parser.add_argument('--color', required=False)
     args = parser.parse_args()
-    
-    print("ArgParse color=" + args.color)
-    
+
+    if args.color:
+        print("Color from command line argument =" + args.color)
+        COLOR = args.color
+        if COLOR_FROM_ENV:
+            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
+    elif COLOR_FROM_ENV:
+        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
+        COLOR = COLOR_FROM_ENV
+    else:
+        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
+
+    # Check if input color is a supported one
+    if COLOR not in color_codes:
+        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
+        exit(1)
+
     # Run Flask Application
     app.run(host="0.0.0.0", port=8080)
